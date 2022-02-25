31db71333690a4239fc840ff7dd0f8913b856561
Johnny Richardson <johnny.c.rich@gmail.com>
2022-02-25 14:54:02 -0500

    deps: add vimeo dependency.

--- admin/components/CustomNav.tsx
+++ admin/components/CustomNav.tsx
@@ -3,7 +3,6 @@ import type { NavigationProps } from '@keystone-6/core/admin-ui/components';
 import { NavigationContainer, NavItem, ListNavItems } from '@keystone-6/core/admin-ui/components';
 
 export function CustomNavigation({ authenticatedItem, lists }: NavigationProps) {
-  debugger
   return (
     <NavigationContainer authenticatedItem={authenticatedItem}>
           <NavItem href="/">Dashboard</NavItem>
