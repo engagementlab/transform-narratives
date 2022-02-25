d14b49c05ee44c1362f68aac4f1118e6f6b65234
Johnny Richardson <johnny.c.rich@gmail.com>
2022-02-25 14:56:57 -0500

    feat: /cms redirection page.

---
+++ pages/cms.tsx
@@ -0,0 +1,14 @@
+
+export default function CMS() {
+  setTimeout(() => {
+    window.location = `https://cms.qa.transformnarratives.org/`;
+  }, 2000); 
+
+  return (
+    <div className="flex justify-center items-center absolute w-full h-full">
+      <h1 className="font-bourbon text-5xl">
+        Sending you to Engagement Lab CMS...
+      </h1>
+    </div>
+  );
+}
