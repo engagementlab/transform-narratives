66c7502cf7f10a50105ad525ebb6b54acea900c6
Johnny Richardson <johnny.c.rich@gmail.com>
2022-03-11 19:49:05 -0500

    feat: more archive layout work.

--- pages/media-archive.tsx
+++ pages/media-archive.tsx
@@ -1,18 +1,21 @@
 import {
     InferGetStaticPropsType
 } from "next";
+import create from 'zustand';
+import _ from 'lodash';
+import { motion, AnimatePresence } from "framer-motion"
+
+
 import {
     query
 } from '.keystone/api';
 import {
     InferRenderersForComponentBlocks
 } from '@keystone-6/fields-document/component-blocks';
-import _ from 'lodash';
 import {
     componentBlocks
 } from '../admin/components/component-blocks';
 import Image from '../components/Image';
-import create from 'zustand';
 
 type Filter = {
     name: string;
@@ -61,8 +64,10 @@ const useStore = create<FilterState>(set => ({
 const RenderFilters = (filters: { [x: string]: any[]; }) => {
     const selectedFilters = useStore(state => state.currentFilters);
     const filtersOpen = useStore(state => state.filtersOpen);
+    const haveFilters = selectedFilters.length > 0;
     const haveFiltersForKey = (key: string) => {return _.intersection(selectedFilters, filters[key]).length > 0};
     const addFilter = useStore(state => state.add);
+    const reset = useStore(state => state.reset);
     const resetForKey = useStore(state => state.remove);
     const toggleOpen = useStore(state => state.toggleOpen);
 
@@ -70,7 +75,7 @@ const RenderFilters = (filters: { [x: string]: any[]; }) => {
                     {Object.keys(filters).map((key) => (
                         <div key={key}>
                             <div className="flex justify-between items-center flex-shrink-0 flex-grow-0">
-                                <svg height="10.0" width="14" id="canvas"><polygon points="0,0 14,0 7.0,9.0" style={{'fill':'#FEF9C7'}}></polygon></svg>
+                                <svg height="10.0" width="14"><polygon points="0,0 14,0 7.0,9.0" style={{'fill':'#FEF9C7'}}></polygon></svg>
                                 {key}
                                 <a href="#" onClick={(e) =>{ resetForKey(filters[key]); e.preventDefault() }} style={{visibility: !haveFiltersForKey(key) ? 'hidden' : 'visible'}}>x</a>
                             </div>
@@ -85,6 +90,8 @@ const RenderFilters = (filters: { [x: string]: any[]; }) => {
 
     return <div> 
             <div className="hidden lg:block">
+                    <a className="uppercase" onClick={(e) =>{ toggleOpen(true); e.preventDefault() }}>Filters</a>   
+                    <a className="uppercase" onClick={(e) =>{ reset(); e.preventDefault() }}  style={{visibility: !haveFilters ? 'hidden' : 'visible'}}>(x) Clear</a> 
                 {menu}     
             </div>
             {/* Mobile/tablet */}
@@ -96,6 +103,7 @@ const RenderFilters = (filters: { [x: string]: any[]; }) => {
 }
 
 const FilterIntersects = (items: any[]) => {
+    
         const selectedFilters = useStore(state => state.currentFilters);
         const haveFilters = selectedFilters.length > 0;
         const reset = useStore(state => state.reset);
@@ -105,22 +113,27 @@ const FilterIntersects = (items: any[]) => {
                 item => selectedFilters.length === 0 ||
                 // ...otherwise, item's filters must match ALL selected filters
                 _.every(selectedFilters, r => _.map(item.filters, 'name').indexOf(r) >= 0));
+
         return <div>
             <div className="w-full flex justify-between">
                     <a className="uppercase" onClick={(e) =>{ toggleOpen(true); e.preventDefault() }}>Filters</a>   
                     <a className="uppercase" onClick={(e) =>{ reset(); e.preventDefault() }}  style={{visibility: !haveFilters ? 'hidden' : 'visible'}}>(x) Clear</a>   
                     <span>Showing {filteredItems.length} Stories</span>
             </div>
-            <div className="md:flex">{
+            <div className="md:flex md:justify-between">{
                         filteredItems.length === 0 ? 
                         <p>No matches!</p> :
-                        filteredItems.map((item, i) => (
-                            <div key={i} className="w-full md:w-1/2 lg:w-1/3">
+                        <AnimatePresence>
+                        {filteredItems.map((item, i) => (
+                            <motion.div key={i} className="w-full md:w-1/2 lg:w-1/3"
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}>
                                     <Image id={`thumb-${i}`} alt={`Thumbnail for media with name "${item.title}"`} imgId={item.thumbnail.publicId} width={235}  />
                                     <p>{item.title}</p>
                                     <p>{item.shortDescription}</p>
                                     <p className="uppercase">{_.map(item.filters, 'name').join(', ')}</p>
-                                </div>))
+                                </motion.div>))}
+                        </AnimatePresence>
                 }
             </div>
         </div>
@@ -132,7 +145,7 @@ export default function MediaArchive({ filtersGrouped, mediaItems }: InferGetSta
         className="container mx-auto mt-14 mb-14 xl:mt-16">
             <div className="w-full"><h2>Media Archive</h2> <hr /></div>
             <div className="flex items-center">
-            <div className='w-1/5 border-r'>
+            <div className='w-1/5 flex-shrink-0 border-r'>
                 {RenderFilters(filtersGrouped)}
             </div>
             <div className="ml-4">
