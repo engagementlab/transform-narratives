0da626413028638cf45d4bea715c3d885afbb036
Johnny Richardson <johnny.c.rich@gmail.com>
2022-03-17 11:58:24 -0400

    feat: Studio page first pass.

--- pages/studios/[key].tsx
+++ pages/studios/[key].tsx
@@ -1,18 +1,20 @@
-import { InferGetStaticPropsType } from 'next';
-import Script from 'next/script'
+import { GetStaticPathsResult, GetStaticPropsContext, InferGetStaticPropsType } from 'next';
+import { DocumentRenderer, DocumentRendererProps } from '@keystone-6/document-renderer';
+import { InferRenderersForComponentBlocks } from '@keystone-6/fields-document/component-blocks'
+import Link from 'next/link';
+import _ from 'lodash';
 
 import { query } from '.keystone/api';
-import { DocumentRenderer } from '@keystone-6/document-renderer';
-import { InferRenderersForComponentBlocks } from '@keystone-6/fields-document/component-blocks';
+
 import Image from '../../components/Image';
 import { componentBlocks } from '../../admin/components/component-blocks';
 
-
 type Studio = {
   id: string;
-  content: any;
   name: string;
-  videos: any[];
+  key: string;
+  filters: any[];
+  content: any;
 };
 
 const componentBlockRenderers: InferRenderersForComponentBlocks<typeof componentBlocks> = {
@@ -24,47 +26,79 @@ const componentBlockRenderers: InferRenderersForComponentBlocks<typeof component
     );
   },
 };
+ 
 
-// Home receives a `studios` prop from `getStaticProps` below
-export default function Home({ studios }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return (
-    <div>
-      <main>
-       
-          {studios.map((studio, i) => (
-              <div key={i}>
-                <h1 className="text-3xl">{studio.name}</h1>
-                <DocumentRenderer key={i} document={studio.content.document} 
-                componentBlocks={componentBlockRenderers} />
+const renderers: DocumentRendererProps['renderers'] = {
+block: {
+  heading: ({ level, children, textAlign }) => {
+    return <p className={`${level === 3 && 'text-2xl text-bluegreen'} font-semibold`} style={{ textAlign }}>{children}</p>;
+  },
+},
+};
 
-                {studio.videos.map((video, v) => (
-                  <div key={v} className='video'>
-                    <p>{video.label}</p>
-                    <div id={"video-embed-"+v}>
-                        <iframe
-                            src={video.value}
-                            frameBorder="0"
-                            allow="autoplay; fullscreen; picture-in-picture"
-                            allowFullScreen
-                        ></iframe>
-                      <Script src="https://player.vimeo.com/api/player.js"></Script>
-                    </div>
+export default function Studio({ item, relatedItems }: InferGetStaticPropsType<typeof getStaticProps>) {
+return (
+    !item ? 'Not found!' :
+  <div>
+      <div className='xl:px-8'>
+          <h1 className="text-3xl">{item.name}</h1>
+          <p>{_.map(item.filters, 'name').join(', ')}</p>
+          <DocumentRenderer document={item.content.document} componentBlocks={componentBlockRenderers} renderers={renderers} />
+          {/* <h3 className='text-2xl text-bluegreen font-semibold'>Explore Related Media</h3> */}
+
+          {relatedItems &&
+              <div>
+                  <div className='flex flex-col lg:flex-row justify-between items-center'>
+                      <p>Browse other stories to keep learning</p>
+                      <Link href='/media-archive' passHref>
+                          <a>
+                              See All
+                          </a>
+                      </Link>
                   </div>
-                ))}
+                  <div className='flex flex-col lg:flex-row'>
+                      {/* {relatedItems.map((relatedItem, i) => (
+                          <Link key={i} href={`/media/${relatedItem.key}`} passHref>
+                              <a className="w-full md:w-1/2 lg:w-1/3">
+                                  <div>
+                                      <Image id={`thumb-${i}`} alt={`Thumbnail for media with name "${relatedItem.name}"`} imgId={relatedItem.thumbnail.publicId} width={302}  />
+                                      <h4 className='text-xl font-semibold mt-3'>{relatedItem.name}</h4>
+                                      
+                                      <p>{_.map(relatedItem.filters, 'name').join(', ')}</p>
+                                  </div>
+                              </a>
+                          </Link>
+                      ))} */}
+                  </div>
               </div>
-          ))}
-       </main>
-    </div>
-  );
+          }
+      </div>
+  </div>
+);
 }
 
-export async function getStaticProps() {
-  const studios = await query.Studio.findMany({ query: 'id name content { document(hydrateRelationships: true) } videos' }) as Studio[];
-  console.log(studios[0].content.document[0].children[0].children[0].children
-    )
+export async function getStaticPaths(): Promise<GetStaticPathsResult> {
+  const items = (await query.Studio.findMany({
+    query: `key`,
+  })) as { key: string }[];
+
+  const paths = items
+    .filter(({ key }) => !!key)
+    .map(({ key }) => `/studios/${key}`);
+
   return {
-    props: {
-      studios
-    }
+    paths,
+    fallback: false,
   };
+}
+
+export async function getStaticProps({ params }: GetStaticPropsContext) {
+  const item = (await query.Studio.findOne({
+      where: { key: params!.key as string },
+      query: 'name filters { name } content { document(hydrateRelationships: true) }',
+  })) as Studio;
+  const relatedItems = (await query.Studio.findMany({
+      query: 'name key filters { type name } thumbnail { publicId }',
+  })) as Studio[];
+  return { props: { item, relatedItems } };
 }
