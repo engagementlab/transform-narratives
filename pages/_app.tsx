58886adf8cede30f40baebe1d451f6d05a3e87fb
Johnny Richardson <johnny.c.rich@gmail.com>
2022-03-28 17:14:55 -0400

    feat: favicon.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head'
 import Header from '../components/Header'
 import Footer from '../components/Footer';
+import Favicon from '../components/Favicon';
 
 function App({ Component, pageProps }: AppProps) {
   return (
@@ -12,6 +13,7 @@ function App({ Component, pageProps }: AppProps) {
         <Head>
           <title>Transforming Narratives of Gun Violence</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <Favicon />
         </Head>
       </div>
       <main className='w-full my-7 mb-24 font-sans'>
