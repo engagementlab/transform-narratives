e959711f2156ea8cd34dd27c6eb9e01ece0c2ea6
Johnny Richardson <johnny.c.rich@gmail.com>
2022-04-06 17:03:17 -0400

    feat: newsletter form WIP.

--- client/pages/get-involved.tsx
+++ client/pages/get-involved.tsx
@@ -12,7 +12,17 @@ type Person = {
     blurb: string;
     image: any;
     content: any;
-}; 
+};
+
+const SubmitEmail = async e => {
+
+  e.preventDefault();
+
+  const email = e.target.email.value;
+  const emailValid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+
+}
+
 export default function Community() {
   return (
     <Layout>
@@ -23,30 +33,41 @@ export default function Community() {
 
               <p>Join our mailing list</p>   
               <div id="newsletter">
-                <form
-                action="https://engagementgamelab.us6.list-manage.com/subscribe/post?u=8cb16e3042072f11cc0680d36&amp;id=e9f56f4f4d"
-                method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate"  
-                target="_blank">
-                <input type="email" placeholder="Your email" name="EMAIL" className="required email" id="mce-EMAIL" width="400" aria-label="Enter your email"/>
-                <input type="submit" value="Subscribe to Newsletter" name="subscribe" id="mc-embedded-subscribe"
+                <form onSubmit={SubmitEmail}>
+                <input type="email" placeholder="Your email" name="EMAIL" id="email" width="400" aria-label="Enter your email" minLength={5} required/>
+                <label>
+                  <input type="submit" value="Subscribe to Newsletter" name="subscribe" id="mc-embedded-subscribe"
                     className="button hidden" aria-hidden="true"/>
+                  <button onClick={SubmitEmail}>
+
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+                  </svg>
+                  </button>
+                </label>
                 </form>
                 <div className="subscribed">
                 <span>
                     Thanks for joining!
                 </span>
                 </div>
-                <button>
-                </button>
+
                 <div className="error">
                     You are already subscribed.
                 </div>
               </div>
-              <p>
-                  <Link href="https://giving.emerson.edu/give-now?fid=h0ZJD8gm3R4%3d&fdesc=i%2bI0v73Km%2bQCb1p7mjPYeYE68k%2f8URMG" passHref>Support our Initiative <svg viewBox="93.105 325.421 10.581 10.581" width="10.581" height="10.581">
-                    <path d="M 101.724 330.494 L 93.908 330.453 L 93.895 331.066 L 101.587 331.066 L 99.436 333.217 L 99.913 333.693 L 102.895 330.725 L 102.881 330.711 L 102.418 330.248 L 99.913 327.729 L 99.436 328.206 Z" fillRule="evenodd" transform="matrix(0.707108, -0.707106, 0.707106, 0.707108, -205.028549, 166.43837)" style="fill: rgb(2, 102, 112);"></path>
-                </svg>
-                </Link></p>
+                <Link href="https://giving.emerson.edu/give-now?fid=h0ZJD8gm3R4%3d&fdesc=i%2bI0v73Km%2bQCb1p7mjPYeYE68k%2f8URMG" passHref>
+                <span className='text-bluegreen'>
+                  Support our Initiative
+                  <svg viewBox="93.105 325.421 10.581 10.581" width="10.581" height="10.581" className='inline'>
+                    <path
+                      d="M 101.724 330.494 L 93.908 330.453 L 93.895 331.066 L 101.587 331.066 L 99.436 333.217 L 99.913 333.693 L 102.895 330.725 L 102.881 330.711 L 102.418 330.248 L 99.913 327.729 L 99.436 328.206 Z"
+                      fillRule="evenodd"
+                      transform="matrix(0.707108, -0.707106, 0.707106, 0.707108, -205.028549, 166.43837)"
+                      style={{"fill": "rgb(2, 102, 112)"}}></path>
+                  </svg>
+                </span>
+                </Link>
     </div>   
     </Layout>
     
