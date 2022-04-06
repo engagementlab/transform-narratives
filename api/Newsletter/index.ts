05321afd18736a0d3afe1355d0a117d910a06ba1
Johnny Richardson <johnny.c.rich@gmail.com>
2022-04-06 14:47:17 -0400

    feat: api dir w/ Newsletter signup azure function.

---
+++ api/Newsletter/index.ts
@@ -0,0 +1,53 @@
+/**
+ * @fileoverview Engagement Lab initiatives content service
+ * @copyright Engagement Lab at Emerson College, 2022
+ *
+ * @author Johnny Richardson
+ * @description
+ *
+ * ==========
+ */
+
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import mailchimp = require('@mailchimp/mailchimp_marketing');
+
+const newsletterSignup: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  const listId = process.env.MAILCHIMP_LIST_ID;
+
+  mailchimp.setConfig({
+    apiKey: process.env.MAILCHIMP_KEY,
+    server: 'us6',
+  });
+
+  try {
+    const response = await mailchimp.lists.addListMember(listId, {
+      email_address: req.query.email,
+      status: 'subscribed',
+    });
+
+    if (response) {
+      context.res = {
+        status: 200,
+        body: 'ok',
+      };
+    }
+  } catch (e) {
+    if (e.response.body.title === 'Member Exists') {
+      context.res = {
+        status: 400,
+        body: 'already_subscribed',
+      };
+      return;
+    }
+
+    context.res = {
+      status: 500,
+      body: e,
+    };
+  }
+};
+
+export default newsletterSignup;
