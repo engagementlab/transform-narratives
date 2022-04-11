782c0300f365d3c014451a68c7bf90f47f4f6253
Johnny Richardson <johnny.c.rich@gmail.com>
2022-04-11 12:59:17 -0400

    fix: Sort all people by first name on community.

--- client/pages/about/community.tsx
+++ client/pages/about/community.tsx
@@ -95,7 +95,7 @@ export async function getStaticProps() {
     query: `values { document } `
   }) as CommunityPage;
   const people = await query.Person.findMany({
-    query: `name title blurb remembrance image { publicId } content { document } `
+    query: `name title blurb remembrance image { publicId } content { document }`, orderBy: {name: 'asc'}
   }) as Person[];
 
   return {
