d39d230cb29a93140c23f1a41d8ff96fcdec6798
Johnny Richardson <johnny.c.rich@gmail.com>
2022-04-13 16:04:10 -0400

    fix: 'client/admin/hooks' fully working.

--- client/admin/schema/hooks.tsx
+++ client/admin/schema/hooks.tsx
@@ -1,17 +1,19 @@
 import { AboutCreateInput, BigPictureCreateInput } from ".keystone/types";
+import _ from 'lodash';
 
 export function CreateKey(name: string) {
     return name.toLocaleLowerCase().replaceAll(/[^a-z+A-Z+0-9+]/ig, '-').replace(/-{2,}/g, '-')
 }
-export const FixButtons = (resolvedData: AboutCreateInput | BigPictureCreateInput) => { 
-      
+export const FixButtons = (resolvedData: AboutCreateInput | BigPictureCreateInput) => {
+
     // Hacky, but works for now to ensure that buttons in content get assigned props
-    // Content may be object or string, it seems  (if object, make copy)
-    let contentParsed = typeof resolvedData.content === 'object' ? JSON.parse(JSON.stringify(resolvedData.content)) : JSON.parse(resolvedData.content as string);
+    // Content may be object or string, it seems (copy it)
+    let contentParsed = typeof resolvedData.content === 'object' ? _.cloneDeep(resolvedData.content) : JSON.parse(resolvedData.content as string);
+
     let parseChildren = (c: any) => {
         if(c.component === 'button') {
             c.props.label = c.children[0].children[0].text;
-            c.props.link = c.children[1].children[0].text;    
+            c.props.link = c.children[1].children[0].text;
         }
         // Recursively move through all children
         c.children.forEach((c2: { children: any; }) => {
@@ -21,6 +23,6 @@ export const FixButtons = (resolvedData: AboutCreateInput | BigPictureCreateInpu
     }
 
     contentParsed.forEach((c: any) => { parseChildren(c) });
-   
-    return typeof resolvedData.content === 'object' ? contentParsed : JSON.stringify(contentParsed); 
+
+    return typeof resolvedData.content === 'object' ? contentParsed : JSON.stringify(contentParsed);
  };
