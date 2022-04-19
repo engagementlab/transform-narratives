407e22693ca9a3d0668e11ff353aff1c95fc1da5
Johnny Richardson <johnny.c.rich@gmail.com>
2022-04-19 12:39:02 -0400

    fix: news item layout fix, show year instead of time, fixed order of posts.

--- client/pages/news/index.tsx
+++ client/pages/news/index.tsx
@@ -26,42 +26,40 @@ export default function News({ items }: InferGetStaticPropsType<typeof getStatic
         <Layout>
             <div className="container mt-14 mb-24 xl:mt-16 px-4 xl:px-8">
                 <h2 className="text-2xl text-bluegreen font-semibold mb-8">Latest News</h2>
-                <div className='flex flex-col mt-6'>
+                <div className='mt-6'>
                     {items.map((item, i) => (
-                        <div className="w-full flex flex-col-reverse md:flex-row" key={i}>
-                        <div className="w-full md:w-1/3">
-                            <div className="text-coated text-xl font-semibold leading-8 mt-3 md:mt-0">
-                                {new Date(item.publishDate).toLocaleDateString('en-US', {
-                                    weekday: 'long',
+                        <div className={`flex flex-col-reverse md:flex-row ${i > 0 && 'mt-14'}`} key={i}>
+                            <div className="w-full md:w-1/3">
+                                <div className="text-coated text-xl font-semibold leading-8 mt-3 md:mt-0">
+                                    {new Date(item.publishDate).toLocaleDateString('en-US', {
+                                        weekday: 'long',
                                     })}
-                                <br />
-                                {new Date(item.publishDate).toLocaleDateString('en-US', {
-                                    month: 'long',
-                                    day: 'numeric',
+                                    <br />
+                                    {new Date(item.publishDate).toLocaleDateString('en-US', {
+                                        month: 'long',
+                                        day: 'numeric',
                                     })}
-                                <br />
-                                {new Date(item.publishDate).toLocaleTimeString('en-US', {
-                                    hour: '2-digit',
-                                    minute: '2-digit',
+                                    <br />
+                                    {new Date(item.publishDate).toLocaleDateString('en-US', {
+                                        year: 'numeric',
                                     })}
+                                </div>
                             </div>
-                        </div>
-                        <div className="flex-grow">
-                            <Link href={`/news/${item.key}`} passHref>
-                                <a>
-                                    <h3 className="text-bluegreen text-xl font-semibold my-2">{item.title}</h3>
-                                </a>
-                            </Link>
-                            <p>{item.blurb}</p>
-
-                            {
+                            <div className="flex-shrink">
+                                <h3 className="text-bluegreen text-xl font-semibold my-2 cursor-pointer">
+                                    <Link href={`/news/${item.key}`} passHref>
+                                    {item.title}
+                                    </Link>
+                                </h3>
+                                <p className='max-w-2xl'>{item.blurb}</p>
+                                {
                                 item.thumbnail ?
                                 <Image id={`thumb-${i}`} alt={`Thumbnail for blog post with title "${item.title}" `}
-                                imgId={item.thumbnail.publicId} width={335} /> :
+                                    imgId={item.thumbnail.publicId} width={335} /> :
                                 <ImagePlaceholder imageLabel='News' width={335} height={200} />
-                            }
+                                }
+                            </div>
                         </div>
-                    </div>
                     ))}
                 </div>
             </div>
@@ -70,7 +68,7 @@ export default function News({ items }: InferGetStaticPropsType<typeof getStatic
 }
 
 export async function getStaticProps() {
-    const items = await query.NewsItem.findMany({ query: 'title key publishDate blurb thumbnail { publicId }'  }) as News[];
+    const items = await query.NewsItem.findMany({ query: 'title key publishDate blurb thumbnail { publicId }', orderBy: {publishDate: 'desc'}  }) as News[];
 
     return {
       props: {
