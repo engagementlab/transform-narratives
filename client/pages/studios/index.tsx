f4e9784241f63d0459cdd6b58ce8f57095d1514b
Johnny Richardson <johnny.c.rich@gmail.com>
2022-04-19 16:28:55 -0400

    fix: studios button language.

--- client/pages/studios/index.tsx
+++ client/pages/studios/index.tsx
@@ -39,7 +39,7 @@ const renderItem = (props: {
                         </div>
                         <button
                             className={btnClass}>
-                            See More</button>
+                            See Details</button>
                     </div>
                 </motion.div>
             </Link>
