e4cb51ab4a933d2befe3bf3edb1cb5242da3259b
Johnny Richardson <johnny.c.rich@gmail.com>
2022-04-21 10:07:06 -0400

    fix: use aspectDefault prop on Image for big picture.

--- client/pages/about/big-picture.tsx
+++ client/pages/about/big-picture.tsx
@@ -1,13 +1,41 @@
 import { InferGetStaticPropsType } from 'next';
 import { query } from '.keystone/api';
 import { DocumentRenderer, DocumentRendererProps } from '@keystone-6/document-renderer';
-import BlockRenderers from '../../components/BlockRenderers';
+// import BlockRenderers from '../../components/BlockRenderers';
 import Layout from '../../components/Layout';
 import HeadingStyle from '../../components/HeadingStyle';
-import FlexLayout from '../../components/FlexLayout';
+import { InferRenderersForComponentBlocks } from '@keystone-6/fields-document/component-blocks';
+import Link from 'next/link';
+import { componentBlocks } from '../../admin/components/component-blocks';
+import Video from '../../components/Video';
+import Image from '../../components/Image';
 
 type BigPicturePage = {
   content: any;
+};
+
+const BlockRenderers: InferRenderersForComponentBlocks<typeof componentBlocks> = {
+  image: (props: any) => {
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <Image id={'img-' + props.image.data.image.publicId} alt={props.image.data.altText} imgId={props.image.data.image.publicId} aspectDefault={true} />
+        <p>{props.image.data.caption}</p>
+      </div>
+    );
+  },
+  video: (props: any) => {
+    return <Video videoLabel={props.video.label} videoUrl={props.video.value} thumbUrl={props.video.thumb} />
+  },
+  button: (props: any) => {
+    return ( 
+      <Link href={props.link.props.node.children[0].text} passHref>
+          <button
+          className='block lg:inline-block rounded-full px-9 py-7 mt-4 uppercase whitespace-nowrap bg-lynx text-bluegreen border-2 border-bluegreen transition-all hover:bg-green-blue hover:text-lynx hover:border-green-blue'>
+          {props.label}
+          </button>
+      </Link>
+    );
+  }
 };
 
 const renderers: DocumentRendererProps['renderers'] = {
