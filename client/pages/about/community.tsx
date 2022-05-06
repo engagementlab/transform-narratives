e21d44df51a059f68a78335f1a7a25e43bc4b90c
Johnny Richardson <johnny.c.rich@gmail.com>
2022-05-06 11:46:00 -0400

    feat: missing person image placeholder.

--- client/pages/about/community.tsx
+++ client/pages/about/community.tsx
@@ -52,7 +52,10 @@ export default function Community({ page, people }: InferGetStaticPropsType<type
                       <div className='flex-shrink-0'>
                           {person.image ?
                             <Image id={`thumb-${i}`} alt={`Thumbnail for person with name "${person.name}"`} imgId={person.image.publicId} width={300} /> :
-                            <ImagePlaceholder imageLabel='Bio' width={300} height={300} />
+                            <svg viewBox="0 0 300 255" width="300" height="255" stroke="#000000" stroke-width="1" stroke-linecap="square" stroke-linejoin="miter" fill="none" color="#000000">
+                              <title>{`Missing image of person with name "${person.name}"`}</title>
+                              <path d="M 57.509 200 C 57.509 165.5 103.509 165.5 126.509 142.5 C 138.009 131 103.509 131 103.509 73.5 C 103.509 35.17 118.838 16 149.509 16 C 180.179 16 195.509 35.17 195.509 73.5 C 195.509 131 161.009 131 172.509 142.5 C 195.509 165.5 241.509 165.5 241.509 200" style={{stroke: 'rgb(141, 51, 210)', strokeOpacity: 0.36, strokeWidth: '7px'}}></path>
+                            </svg>
                           }
                       </div>
                       <div className='lg:ml-6 w-full lg:w-1/2 xl:w-1/3'>
