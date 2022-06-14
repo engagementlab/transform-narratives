85ae48c4ee0ccf4c7dfccb6b0b29fd1a947d15bb
ELDevBot <engagementlab@emerson.edu>
2022-06-14 18:44:06 +0000

    Automated generated static production app (GH action #110) [skip ci]

---
+++ client/out/_next/static/BWWKQpw43GAEhmMYx70fV/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcms","\u002Fabout\u002Fbig-picture","\u002Fabout\u002Fcommunity","\u002Fabout\u002Finitiative","\u002Fevents","\u002F","\u002Fnews","\u002Farchive","\u002Fstudios","\u002Farchive\u002F[key]","\u002Fevents\u002F[key]","\u002Fnews\u002F[key]","\u002Fstudios\u002F[key]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
