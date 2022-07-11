12574d6000bb9349c0f350ae91ad6f285d5c8bc4
Johnny Richardson <johnny.c.rich@gmail.com>
2022-07-11 14:06:10 -0400

    refactor(tngvi): rollback use of preconstruct.

--- apps/tngvi/next.config.js
+++ apps/tngvi/next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 // const { withKeystone } = require('@keystone-6/core/next');
-const withPreconstruct = require('@preconstruct/next');
-const nextConfig = withPreconstruct({
+// const withPreconstruct = require('@preconstruct/next');
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['i.vimeocdn.com'],
     loader: 'custom',
   },
   trailingSlash: true,
-});
+};
 
 module.exports = nextConfig;
