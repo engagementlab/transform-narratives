b38b7d0282697169e82fee31a96aeebcfe93c237
Johnny Richardson <johnny.c.rich@gmail.com>
2022-07-13 16:24:48 -0400

    refactor(tngvi): re-apply use of preconstruct in next config.

--- apps/tngvi/next.config.js
+++ apps/tngvi/next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 // const { withKeystone } = require('@keystone-6/core/next');
-// const withPreconstruct = require('@preconstruct/next');
-const nextConfig = {
+const withPreconstruct = require('@preconstruct/next');
+// const withTM = require('next-transpile-modules')(['@el-next/components']);
+const nextConfig = withPreconstruct({
   reactStrictMode: true,
   images: {
     domains: ['i.vimeocdn.com'],
     loader: 'custom',
   },
   trailingSlash: true,
-};
+});
 
 module.exports = nextConfig;
